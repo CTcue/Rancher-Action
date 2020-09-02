0e478add7770110e207e66d177ebdd6d0e24c569
TimVanMourik <timvanmourik@gmail.com>
2020-09-02 15:56:25 +0200

    use `command_string` instead of `args`

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@
 token="$INPUT_TOKEN"
 context="$INPUT_CONTEXT"
 url="$INPUT_URL"
+command_string=$INPUT_COMMAND_STRING
+
+echo $command_string
 
 rancher login --token $token --context $context $url
-rancher kubectl $*
+rancher kubectl $command_string
