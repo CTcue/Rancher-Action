8d0507af9e7ee2d8e537f42d2be450fae665e528
Bernd van der Veen <bernd@ctcue.com>
2020-09-03 08:12:28 +0200

    Remove debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,27 +1,12 @@
 #!/bin/sh -l
 
-echo INIT
-
 token="$INPUT_TOKEN"
 context="$INPUT_CONTEXT"
 url="$INPUT_URL"
-command_string='$INPUT_COMMAND_STRING'
 
-echo START
-
-echo "CMD STR $command_string"
-echo "CMD STR INPUT $INPUT_COMMAND_STRING"
-
-echo POST STR
-
-command_to_execute="rancher --debug kubectl $INPUT_COMMAND_STRING"
-
-echo "CMD EXEC $command_to_execute"
+command_to_execute="rancher kubectl $INPUT_COMMAND_STRING"
 
 rancher login --token $token --context $context $url
 
-echo POST LOGIN
-
 eval $command_to_execute
 
-echo POST EVAL
