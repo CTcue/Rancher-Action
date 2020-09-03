54e2d4c297b2272c4de2a9567ace288eaaa56496
Bernd van der Veen <bernd@ctcue.com>
2020-09-03 07:40:15 +0200

    debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,26 @@
 #!/bin/sh -l
 
+echo INIT
+
 token="$INPUT_TOKEN"
 context="$INPUT_CONTEXT"
 url="$INPUT_URL"
 command_string='$INPUT_COMMAND_STRING'
 
-echo CMD STR $command
+echo START
+
+echo CMD STR $command_string
+
+echo POST STR
+
 command_to_execute="rancher --debug kubectl $command_string"
 
 echo CMD EXEC $command_to_execute
 
 rancher login --token $token --context $context $url
+
+echo POST LOGIN
+
 eval $command_to_execute
+
+echo POST EVAL
